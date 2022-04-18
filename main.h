98ab1dd0af03f6770c8ffc9e41e211615d70b812
Tolexy95 <toyin95.olubayo@gmail.com>
2022-04-18 08:36:37 -0700

    update

--- main.h
+++ main.h
@@ -1,36 +1,41 @@
-#ifndef MAIN_H_
-#define MAIN_H_
-
-#include <stdlib.h>
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 #include <stdarg.h>
-#include <unistd.h>
+int _printf(const char *format, ...);
+int print_char(va_list ap);
+int print_str(va_list ap);
+int print_nbr(va_list ap);
+int print_binary(va_list ap);
+int print_octal(va_list ap);
+int print_hexa_lower(va_list ap);
+int print_hexa_upper(va_list ap);
+int print_unsigned(va_list ap);
+int print_str_unprintable(va_list ap);
+int print_str_reverse(va_list ap);
+int print_ptr(va_list ap);
+int print_rot13(va_list ap);
+int print_percent(va_list ap __attribute__((unused)));
+int print_number(int n);
+int print_unsigned_number(unsigned int n);
+int _putchar(char c);
+int _puts(char *str, int ascii);
+int _nbr_len(int prmNumber);
+int _strlen_recursion(char *s);
+int convert_alpha_numeric(int nb, int upper);
+char *convert_rot13(char *str);
+char *convert_base(unsigned long nb, unsigned int base, int upper);
+char *_strdup(char *str);
+char *convert_base_pointer(unsigned long p);
 
 /**
- * struct convert - Entry point
- *
- * Description: Defines a structure for symbols and functions
- *
- * @sym: The operator
- * @f: The function associated
+ * struct flags_printf - struct conversion to function
+ * @c: flag string
+ * @f: pointer to func
  */
 
-struct convert
+typedef struct flags_printf
 {
-
-	char *sym;
+	char *c;
 	int (*f)(va_list);
-
-};
-typedef struct convert con_vert;
-
-
-int _putchar(char);
-int parser(const char *format, con_vert f_list[], va_list arg_list);
-int _printf(const char *format, ...);
-int print_char(va_list);
-int print_string(va_list);
-int print_percent(va_list);
-int print_integer(va_list);
-int print_number(va_list);
-
-#endif /*MAIN_H_*/
+} flags_p;
+#endif
